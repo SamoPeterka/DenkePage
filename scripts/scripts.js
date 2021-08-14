f02f3f3d9175932edfca6ad1789cb6bd31b60587
SamoPeterka <samo.peterka@gmail.com>
2021-08-14 14:16:11 +0200

    formsubmitcustomcommented

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -99,17 +99,17 @@ $(document).ready(function(){
       });
 
 
-      document.querySelector("form").addEventListener("submit", handleSubmit);
+      // document.querySelector("form").addEventListener("submit", handleSubmit);
 
-      const handleSubmit = (e) => {
-        e.preventDefault()
-        let myForm = document.getElementById('contact-form');
-        let formData = new FormData(myForm)
-        fetch('/', {
-          method: 'POST',
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(formData).toString()
-        }).then(() => console.log('Form successfully submitted')).catch((error) =>
-          alert(error))
-      }
+      // const handleSubmit = (e) => {
+      //   e.preventDefault()
+      //   let myForm = document.getElementById('contact-form');
+      //   let formData = new FormData(myForm)
+      //   fetch('/', {
+      //     method: 'POST',
+      //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      //     body: new URLSearchParams(formData).toString()
+      //   }).then(() => console.log('Form successfully submitted')).catch((error) =>
+      //     alert(error))
+      // }
 });
