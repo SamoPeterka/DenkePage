efb805bb691bcab2400295f2cfe5242e79f5ccb7
SamoPeterka <samo.peterka@gmail.com>
2021-08-17 20:49:58 +0200

    slike pa to

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,6 +25,7 @@ $(document).ready(function(){
         slideTransition: 'linear',
         autoplayHoverPause: false,
         dots: false,
+        lazyload: true,
     });
 
     //Hide navbar on scroll and change color
